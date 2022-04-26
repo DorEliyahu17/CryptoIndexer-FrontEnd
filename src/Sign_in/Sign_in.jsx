a7d76f371dcdb0b62760e0a75791b29f88987290
matan8919 <matankalili8919@gmail.com>
2022-04-26 11:56:00 +0300

    add sign in + sign up pages, change the app-bar

---
+++ src/Sign_in/Sign_in.jsx
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from "react";
+import PropTypes from 'prop-types';
+
+
+
+function Sign_in() {
+  return (
+    <div>
+      <h1>Sign_in</h1>
+    </div>
+  );
+}
+
+export default Sign_in; 
