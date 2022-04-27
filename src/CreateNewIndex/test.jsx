894c64a27187c40f48c9fc67c7f63f8347c64de2
matan8919 <matankalili8919@gmail.com>
2022-04-28 01:52:46 +0300

    finished do logs about symbols and percents input. finished the fiture add\realse area buttoms

--- src/CreateNewIndex/test.jsx
+++ src/CreateNewIndex/test.jsx
@@ -1,11 +1,42 @@
-import * as React from 'react';
+// Matan and Dor write
+
+import React, {useEffect, useState, Fragment} from "react";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import PropTypes from 'prop-types';
 
-export default function ValidationTextFields() {
-  return (
+function CreateNewIndex() {
+  const [ symbolsList, setSymbolList ] = useState([{name: '', percent: 0}, {name: '', percent: 0}]);
+
+  useEffect(() => {
+    symbolsList.map((symbol, index) => {
+      console.log("symbol.name="+symbol.name+"symbol.percent="+symbol.percent+" index="+index);
+    });
+  }, [symbolsList])
+
+  const handleOnClickAddSymbol = () => {
+    let changedSymbolsList = symbolsList;
+    changedSymbolsList.push({name: '', percent: 0});
+    setSymbolList(changedSymbolsList);
+  };
+
+  const handleOnChangeSymbol = (event, index) => {
+    let changedSymbolsList = symbolsList;
+    changedSymbolsList[index].name = event.target.value; 
+    setSymbolList(changedSymbolsList);
+  };
+
+  const handleOnChangePercent = (event, index) => {
+    let changedSymbolsList = symbolsList;
+    changedSymbolsList[index].percent = event.target.value;
+    setSymbolList(changedSymbolsList);
+  };
+
+  const renderNewSymbolPercentLine = (boxIndex, symbolName = '', percent = 0) => (
     <Box
       component="form"
+      id={`box-${boxIndex}`}
       sx={{
         '& .MuiTextField-root': { m: 1, width: '25ch' },
       }}
@@ -14,36 +45,43 @@ export default function ValidationTextFields() {
     >
       <div>
         <TextField
-          error
-          id="outlined-error"
-          label="Error"
-          defaultValue="Hello World"
+          required
+          // id="outlined-required"
+          id={`symbol-${boxIndex}`}
+          label="Symbol Name"
+          placeholder="Symbol Name"
+          // value={symbolName}
+          onChange={(event) => handleOnChangeSymbol(event, boxIndex)}
         />
         <TextField
-          error
-          id="outlined-error-helper-text"
-          label="Error"
-          defaultValue="Hello World"
-          helperText="Incorrect entry."
-        />
-      </div>
-      <div>
-        <TextField
-          error
-          id="filled-error"
-          label="Error"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          error
-          id="filled-error-helper-text"
-          label="Error"
-          defaultValue="Hello World"
-          helperText="Incorrect entry."
-          variant="filled"
+          required
+          // id="outlined-required"
+          id={`percent-${boxIndex}`}
+          label="Percent"
+          placeholder="Percent"
+          // value={percent}
+          onChange={(event) => handleOnChangePercent(event, boxIndex)}
         />
       </div>
     </Box>
   );
+
+  return (
+    <div id="create-new-index-form">
+      <div id="create-new-index-symbol-list">
+        {/* {(symbolsList.length == 0) ?
+          renderNewSymbolPercentLine(0)
+        : */}
+          {symbolsList.map((symbol, index) => {
+            console.log("symbol.name="+symbol.name+"symbol.percent="+symbol.percent+" index="+index);
+            return renderNewSymbolPercentLine(index, symbol.name, symbol.percent);
+          })}
+        {/* } */}
+      </div>
+      <Button variant="contained" id="add-new-symbol" onClick={handleOnClickAddSymbol}>
+        Add
+      </Button>
+    </div>
+  );
 }
+export default CreateNewIndex; ``
