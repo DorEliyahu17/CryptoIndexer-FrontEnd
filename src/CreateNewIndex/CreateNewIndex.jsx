1d86cc8cb82374d6cacd0d0f6b699bbcb3b407fc
Dor Eliyahu <doreliyahu17@gmail.com>
2022-05-10 03:50:21 +0300

    downgrade from react 18 to 17

--- src/CreateNewIndex/CreateNewIndex.jsx
+++ src/CreateNewIndex/CreateNewIndex.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, Fragment} from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
@@ -10,11 +10,11 @@ import { unstable_composeClasses } from "@mui/material";
 import { ClassNames } from "@emotion/react";
 
 function CreateNewIndex() {
-  
-  const [listSymbolPercentLine, setlistSymbolPercentLine] =  useState([{name: '', percent: 0}, {name: '', percent:0}]);
-  const [disableButtomBackTestSymbol,setdisableButtomBackTestSymbol] = useState({state: true});
-  const [disableButtomBackTestPercent,setdisableButtomBackTestPercent] = useState({state: true});
-  
+
+  const [listSymbolPercentLine, setlistSymbolPercentLine] = useState([{ name: '', percent: 0 }, { name: '', percent: 0 }]);
+  const [disableButtomBackTestSymbol, setdisableButtomBackTestSymbol] = useState({ state: true });
+  const [disableButtomBackTestPercent, setdisableButtomBackTestPercent] = useState({ state: true });
+
   const renderSymbolPercentLine = (boxIndex) => (
     <Box
       component="form"
@@ -24,40 +24,40 @@ function CreateNewIndex() {
       }}
       autoComplete="off"
     >
-      <InputMask 
-        mask="aaa" 
+      <InputMask
+        mask="aaa"
         disabled={false}
         maskChar=" "
         id={`symbol-${boxIndex}`}
         onChange={(event) => handleOnChangeSymbol(event, boxIndex)}
-        > 
+      >
         {() => <TextField
-            required
-            label="Symbol Name"
-            placeholder="Symbol Name"/>}
+          required
+          label="Symbol Name"
+          placeholder="Symbol Name" />}
       </InputMask>
-      <InputMask 
-        mask="99" 
+      <InputMask
+        mask="99"
         disabled={false}
         maskChar=" "
         id={`percent-${boxIndex}`}
         onChange={(event) => handleOnChangePercent(event, boxIndex)}>
-      {() => <TextField
-        required
-        label="0"
-        placeholder="Percent"/>}
+        {() => <TextField
+          required
+          label="Percent"
+          placeholder="Percent" />}
       </InputMask>
     </Box>
   );
 
-  const handleOnClickAdd = () =>{
+  const handleOnClickAdd = () => {
     let changedlistSymbolPercentLine = listSymbolPercentLine.map((listItem) => {
       return listItem;
     });
-    changedlistSymbolPercentLine.push({name: '', percent: 0})
+    changedlistSymbolPercentLine.push({ name: '', percent: 0 })
     setlistSymbolPercentLine(changedlistSymbolPercentLine);
   };
-  const handleOnClickRealse = () =>{
+  const handleOnClickRealse = () => {
     let changedlistSymbolPercentLine = listSymbolPercentLine.map((listItem) => {
       return listItem;
     });
@@ -66,23 +66,23 @@ function CreateNewIndex() {
   }
   const handleOnChangeSymbol = (event, index) => {
     let changedSymbolsList = listSymbolPercentLine;
-    changedSymbolsList[index].name = event.target.value; 
+    changedSymbolsList[index].name = event.target.value;
     setlistSymbolPercentLine(changedSymbolsList);
-    console.log("NewSymbol.name= "+listSymbolPercentLine[index].name+" Symbol.percent="+listSymbolPercentLine[index].percent+" LineIndex="+index);
+    console.log("NewSymbol.name= " + listSymbolPercentLine[index].name + " Symbol.percent=" + listSymbolPercentLine[index].percent + " LineIndex=" + index);
     check_if_all_symbol_complete()
   };
   const check_if_all_symbol_complete = () => {
-    listSymbolPercentLine.map((stock,index) => {
-      (stock.name !== '' && stock.name !== null) ? setdisableButtomBackTestSymbol({state:false}) : setdisableButtomBackTestSymbol({state:true});
+    listSymbolPercentLine.map((stock, index) => {
+      (stock.name !== '' && stock.name !== null) ? setdisableButtomBackTestSymbol({ state: false }) : setdisableButtomBackTestSymbol({ state: true });
     })
-    console.log("disableButtomBackTestSymbol state is: " +disableButtomBackTestSymbol.state)
+    console.log("disableButtomBackTestSymbol state is: " + disableButtomBackTestSymbol.state)
   }
 
   const handleOnChangePercent = (event, index) => {
     let changedSymbolsList = listSymbolPercentLine;
     changedSymbolsList[index].percent = event.target.value;
     setlistSymbolPercentLine(changedSymbolsList);
-    console.log("Symbol.name="+listSymbolPercentLine[index].name+" NewSymbol.percent="+listSymbolPercentLine[index].percent+" LineIndex="+index);
+    console.log("Symbol.name=" + listSymbolPercentLine[index].name + " NewSymbol.percent=" + listSymbolPercentLine[index].percent + " LineIndex=" + index);
     check_if_all_percent_complete()
   };
   const check_if_all_percent_complete = () => {
@@ -90,38 +90,40 @@ function CreateNewIndex() {
     listSymbolPercentLine.map((stock, index) => {
       sumPercent += Number(stock.percent);
     })
-    if(sumPercent === 100) {
-      setdisableButtomBackTestPercent({state:false})}
-    else {
-      setdisableButtomBackTestPercent({state:true})}
+    if (sumPercent === 100) {
+      setdisableButtomBackTestPercent({ state: false })
     }
+    else {
+      setdisableButtomBackTestPercent({ state: true })
+    }
+  }
 
   return (
     <div id="create-new-index-form">
       <div id="create-new-index-symbol-list">
-          {listSymbolPercentLine.map((symbol, index) => {
-            console.log("Symbol.name="+symbol.name+"Symbol.percent="+symbol.percent+" LineIndex="+index);
-            return renderSymbolPercentLine(index);
-          })}
+        {listSymbolPercentLine.map((symbol, index) => {
+          console.log("Symbol.name=" + symbol.name + "Symbol.percent=" + symbol.percent + " LineIndex=" + index);
+          return renderSymbolPercentLine(index);
+        })}
       </div>
-        <Button variant="contained" id="add-new-SymbolPercentLine" onClick={handleOnClickAdd}>
-          Add
-        </Button>
-         { listSymbolPercentLine.length > 2 
-        ? 
-          <Stack direction="row" spacing={2}>
-            <Button variant="outlined" startIcon={<DeleteIcon />} id="Relase-last-SymbolPercentLine" onClick={handleOnClickRealse}>
+      <Button variant="contained" id="add-new-SymbolPercentLine" onClick={handleOnClickAdd}>
+        Add
+      </Button>
+      {listSymbolPercentLine.length > 2
+        ?
+        <Stack direction="row" spacing={2}>
+          <Button variant="outlined" startIcon={<DeleteIcon />} id="Relase-last-SymbolPercentLine" onClick={handleOnClickRealse}>
             Relase
-            </Button>
-          </Stack>
+          </Button>
+        </Stack>
         : null
-         }
-         <div> 
-         <Button disabled={disableButtomBackTestSymbol.state||disableButtomBackTestPercent.state} variant="contained" id="BackTestButtom" /*onClick={handleOnClickAdd}*/>
+      }
+      <div>
+        <Button disabled={disableButtomBackTestSymbol.state || disableButtomBackTestPercent.state} variant="contained" id="BackTestButtom" /*onClick={handleOnClickAdd}*/>
           Backtest
-        </Button> 
-         </div>
+        </Button>
+      </div>
     </div>
-    )
+  )
 };
 export default CreateNewIndex;
