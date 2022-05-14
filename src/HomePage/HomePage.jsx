97249702891a1a6ee18ad06eacf4c2624f48748e
TalGavriel <talgavriel1@gmail.com>
2022-05-14 19:53:53 +0300

    change the mui table module

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,21 +1,55 @@
 import React, { useEffect, useState } from "react";
 // import ReactDOM from "react-dom/client";
 import PropTypes from 'prop-types';
-import { Table } from "@mui/material";
-import { ThemeProvider, createTheme } from '@mui/material';
+import { withStyles } from '@material-ui/core/styles';
+//import Table from '@material-ui/core/Table';
+import { ThemeProvider, createTheme, IconButton } from '@mui/material';
 import { ButtonUnstyled } from "@mui/base";
 import BuyOrSellModal from "../ModalComponent/BuyOrSellModal";
+
+//mui-table
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import TablePagination from '@material-ui/core/TablePagination';  
+import TableContainer from '@material-ui/core/TableContainer';  
+import TextField from  '@material-ui/core/TextField';
+
+
+//MUIDataTable
+import MUIDataTable from "mui-datatables";
+
+
+
 //mu-icons
 
 //import Remove from '@material-ui/icons/Remove';
-//import { Delete } from "@material-ui/icons";
+import { Delete } from "@material-ui/icons";
 import { Payment } from "@material-ui/icons";
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-import MaterialTable from "material-table";
+//import MaterialTable from "material-table";
 
 import './HomePage.less';
 
-function HomePage() {
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'hide',
+  },
+  table: {
+    minWidth: 340,
+  },
+  tableCell: {
+    paddingRight: 4,
+    paddingLeft: 5
+  }
+});
+
+function HomePage(props) {
   const [example, setExample] = useState(false);
   const [SymbolsData, setSymbolsData] = useState([]);
   const defaultMaterialTheme = createTheme();
@@ -24,17 +58,14 @@ function HomePage() {
   const [showSellDialog, setShowSellDialog] = useState(false);
   const [sellIndexInput, setSellIndexInput] = useState({ indexName: "", countToSell: 0 });
 
+  //const classes = useStyles();  
+  const [page, setPage] = React.useState(0);  
+  const [data, setData] = useState([]);  
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);  
+
   // const tableRef = React.createRef();
 
-  const symbolColumns = [
-    { field: 'symbol', title: 'Symbol' },
-    { field: 'price', title: 'Price' },
-    { field: 'daysGain', title: 'Days gain(%)' },
-    { field: 'dayLow', title: 'Day Low' },
-    { field: 'dayHigh', title: 'Day High' },
-    { field: 'marketCap', title: 'Market Cap' },
-    { field: 'volume', title: 'Volume', type: "numeric" },
-  ];
+
 
 
   // const symbolData = (query) => (
@@ -54,37 +85,6 @@ function HomePage() {
   //   }));
 
 
-
-  const symbolData = [
-    {
-      "symbol": "BTC",
-      "price": "$40,038.60",
-      "daysGain": "2.28%",
-      "dayLow": "$40,028.60",
-      "dayHigh": "$40,138.60",
-      "marketCap": "$761,877,839,620",
-      "volume": "$33,368,145,255"
-    },
-    {
-      "symbol": "ETH",
-      "price": "$2,956.03",
-      "daysGain": "2.97%	",
-      "dayLow": "$2,950.00",
-      "dayHigh": "$2,986.03",
-      "marketCap": "$356,678,709,989",
-      "volume": "$18,253,003,101"
-    }
-  ];
-
-
-  const indexColumns = [
-    { field: 'indexName', title: 'Index Name' },
-    { field: 'recommendedRating', title: 'Recommended rating' },
-    { field: 'daysGain', title: 'Days gain(%)' },
-    { field: 'usersCount', title: 'Users Count' }
-  ];
-
-
   // const commonIndexData = (query) => (
   //   new Promise((resolve,reject)=> {
   //     let url = "https://server/api/get/index?";
@@ -100,23 +100,6 @@ function HomePage() {
   //           });
   //         });
   //   }));
-
-
-  const commonIndexesData = [
-    {
-      "indexName": "index1",
-      "recommendedRating": "High",
-      "daysGain": "2.28%",
-      "usersCount": "50"
-    },
-    {
-      "indexName": "index2",
-      "recommendedRating": "Low",
-      "daysGain": "2.97%	",
-      "usersCount": "2"
-    }
-  ];
-
 
 
   // const ownIndexData = (query) => (
@@ -135,23 +118,99 @@ function HomePage() {
   //         });
   //   }));
 
+  //COLUMNS
 
-  const ownIndexesData = [
-    {
-      "indexName": "index1",
-      "recommendedRating": "High",
-      "daysGain": "2.28%",
-      "usersCount": "50"
+  const symbolColumns = ['Symbol','Price','Days gain(%)','Day Low','Day High', 'Market Cap','Volume' ];
+
+  const commonIndexColumns = [
+    {name: "Payment",options: {customBodyRender: (value, tableMeta, updateValue) => {
+      return (
+        <button onClick={() => {HandlePaymentRow(tableMeta)}}>
+            <Payment color="primary" />
+        </button>
+      );
+    } }},
+  'Index Name','Recommended rating','Days gain(%)','Users Count'];
+
+  const ownIndexColumns = [
+    {name: "Payment",options: {customBodyRender: (value, tableMeta, updateValue) => {
+      return (
+        <button onClick={() => {HandlePaymentRow(tableMeta)}}>
+            <Payment color="primary" />
+        </button>
+      );
+    } }},
+    {name: "Sell",options: {customBodyRender: (value, tableMeta, updateValue) => {
+      return (
+        <button onClick={() => {HandleSellRow(tableMeta)}}>
+          <HighlightOffIcon color="secondary"/>
+        </button>
+      );
+    } }},
+  'Index Name','Recommended rating','Days gain(%)','Users Count'];
+
+
+  //OPTIONS
+  const symbolOptions = {
+    //filterType: "checkbox",        
+    rowsPerPage:[3],
+    rowsPerPageOptions:[3,5,10,15],
+    selectableRowsHideCheckboxes: true,
+    onChangePage (currentPage) {
+      console.log({currentPage});
     },
-    {
-      "indexName": "index2",
-      "recommendedRating": "Low",
-      "daysGain": "2.97%	",
-      "usersCount": "2"
+    onChangeRowsPerPage (numberOfRows) {
+      console.log({numberOfRows});
     }
+  };
+
+  const ownIndexesOptions = {
+    //filterType: "checkbox",        
+    rowsPerPage:[3],
+    rowsPerPageOptions:[3,5,10,15],
+    selectableRowsHideCheckboxes: true,
+    onChangePage (currentPage) {
+      console.log({currentPage});
+    },
+    onChangeRowsPerPage (numberOfRows) {
+      console.log({numberOfRows});
+    }
+  };
+
+  const commonIndexesOptions = {
+    //filterType: "checkbox",        
+    rowsPerPage:[3],
+    rowsPerPageOptions:[3,5,10,15],
+    selectableRowsHideCheckboxes: true,
+    onChangePage (currentPage) {
+      console.log({currentPage});
+    },
+    onChangeRowsPerPage (numberOfRows) {
+      console.log({numberOfRows});
+    }
+  };
+
+//DATA
+
+  const symbolData = [
+    ["BTC", "$40,038.60","2.28%", "$40,028.60", "$40,138.60","$761,877,839,620","$33,368,145,255"],
+    ["ETH", "$2,956.03","2.97%", "$2,950.00", "$2,986.03","$356,678,709,989","$18,253,003,101"]
   ];
 
-  const HandlePaymentRow = (event, rowData) => {
+
+const commonIndexesData = [
+  [,"index1", "High","2.28%", "50"],
+  [,"index2", "Low","2.97%", "2"]
+];
+
+const ownIndexesData = [
+  [,,"index1", "High","2.28%", 50],
+  [,,"index2", "Low","2.97%", 2]
+];
+
+
+
+  const HandlePaymentRow = (rowData) => {
     console.log(rowData.indexName);
     buyIndexInput.indexName = rowData.indexName;
     setShowBuyDialog(true);
@@ -168,15 +227,17 @@ function HomePage() {
     return Promise.resolve();
   }
 
-  const HandleSellRow = (event, rowData) => {
-    console.log(rowData.indexName);
-    sellIndexInput.indexName = rowData.indexName;
+
+  const HandleSellRow = (rowData) => {
+    console.log(rowData.rowData);
+    sellIndexInput.indexName = rowData[0];
     setShowSellDialog(true);
     console.log(sellIndexInput.countToSell);
     //todo: send the rowData.indexName and sellIndexInput.countToBuy to SELL api
     //sellIndexInput.countToSell = 0;
     //sellIndexInput.indexName = "";
   }
+
   const SellIndex = (countToSell) => {
     console.log(sellIndexInput.indexName);
     console.log(countToSell);
@@ -184,11 +245,24 @@ function HomePage() {
     return Promise.resolve();
   }
 
-  const handleGetSymbolsData = () => {
-    //todo:
-    //get the data from the backed
-    // setSymbolsData(res);
-  };
+  // const handleGetSymbolsData = () => {
+  //   //todo:
+  //   //get the data from the backed
+  //   // setSymbolsData(res);
+  // };
+
+  // const handleChangePage = (event, newPage) => {  
+  //   setPage(newPage);  
+  // };  
+  
+  // const handleChangeRowsPerPage = event => {  
+  //   setRowsPerPage(+event.target.value);  
+  //   setPage(0);  
+  // }; 
+
+// const handleChangePaymentRow = (event, buyIndexRow) => {  
+//   setBuyIndexInput(buyIndexRow);  
+// };  
 
 
 
@@ -205,35 +279,28 @@ function HomePage() {
       <div>
         <h1>Home Page</h1>
 
-        <ThemeProvider theme={defaultMaterialTheme}>
-          <MaterialTable title="Symbols Table" columns={symbolColumns} data={symbolData} options={{ exportButton: true }} />;
-        </ThemeProvider>
 
+        <MUIDataTable
+          title={"Symbols Table"}
+          data={symbolData}
+          columns={symbolColumns}
+          options={symbolOptions}
+        />
 
-        <ThemeProvider theme={defaultMaterialTheme}>
-          <MaterialTable title="Best Indexes Table" columns={indexColumns} data={commonIndexesData} actions={[
-            {
-              icon: Payment,
-              tooltip: 'pay to buy it',
-              onClick: (event, rowData) => HandlePaymentRow(event, rowData)
-            }
-          ]} />;
-        </ThemeProvider>
+        <MUIDataTable
+          title={"Best Indexes Table"}
+          data={commonIndexesData}
+          columns={commonIndexColumns}
+          options={commonIndexesOptions}
+        />
 
-        <ThemeProvider theme={defaultMaterialTheme}>
-          <MaterialTable title="My Own Indexes Table" columns={indexColumns} data={ownIndexesData} actions={[
-            {
-              icon: Payment,
-              tooltip: 'pay to buy it',
-              onClick: (event, rowData) => HandlePaymentRow(event, rowData)
-            },
-            {
-              icon: HighlightOffIcon,
-              tooltip: 'sell this index',
-              onClick: (event, rowData) => HandleSellRow(event, rowData)
-            }
-          ]} />;
-        </ThemeProvider>
+        <MUIDataTable
+          title={"Own Indexes Table"}
+          data={ownIndexesData}
+          columns={ownIndexColumns}
+          options={ownIndexesOptions}
+        />
+
 
       </div>
       {showBuyDialog && (
