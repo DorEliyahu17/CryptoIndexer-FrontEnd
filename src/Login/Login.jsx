a9b2cc352548616af211cc62e1040381581637b8
Dor Eliyahu <doreliyahu17@gmail.com>
2022-05-24 19:22:55 +0300

    fixed noa changes

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -90,16 +90,11 @@ export default function SignIn() {
               sx={{ mt: 3, mb: 2 }}
             >
               Sign In
-            
+
             </Button>
             <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/NewAccount" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
